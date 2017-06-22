54eaa0023f1c8d74d66039abf8b0434fc23a5b09
Kin Chan <kicha@microsoft.com>
2017-06-22 16:17:26 -0700

    tensorBoard visualization sample

---
+++ tensorBoard/readme.md
@@ -0,0 +1,7 @@
+run with
+
+python run.py
+
+then launch tensorboard with
+
+tensorboard --logdir=/dmhack2017/tensorBoard/logs
---
+++ tensorBoard/run.py
@@ -0,0 +1,27 @@
+import tensorflow as tf 
+import numpy
+import pandas as pd
+df=pd.read_csv('/dmhack2017/tensorBoard/tensorflow_test_input.tsv', sep='\t', header=None)
+d = df.values
+l = pd.read_csv('/dmhack2017/tensorBoard/tensorflow_test_label.tsv', sep='\t', header=None)
+labels = l.values
+data = numpy.float32(d)
+labels = numpy.array(l,'str')
+#print data, labels
+
+log_dir = '/dmhack2017/tensorBoard/logs'
+max_steps = 10000
+
+#tensorflow
+sess = tf.InteractiveSession()
+
+embedding = tf.Variable(tf.stack(data), trainable=False, name="embedding")
+
+tf.global_variables_initializer().run()
+
+saver = tf.train.Saver()
+writer = tf.summary.FileWriter(log_dir + '/projector', sess.graph)
+
+saver.save(sess, log_dir+'/model.ckpt', global_step=max_steps)
+
+
