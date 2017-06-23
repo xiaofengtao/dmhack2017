458eb0e275783d589d29d08d5264df8d3d876075
maschram <maschram@microsoft.com>
2017-06-23 08:40:11 -0700

    Update trainmodelfromqueries.py

--- trainmodelfromqueries.py
+++ trainmodelfromqueries.py
@@ -3,7 +3,7 @@ from nltk.corpus import stopwords
 
 workpath = "c:\\users\\maschram\\downloads\\"
 usephrase = False
-outsize=200
+outsize=300
 minimum=10
 workers=4
 txtoutput = True
@@ -19,10 +19,12 @@ class queryiterator(object):
                 for phrase in line.split("\t")[0].split("|"):
                     keywords = phrase.split(" ")
                     keywords = [k for k in keywords if k not in self.stop]
+                    keywords = [k for k in keywords if len(k)>3]
                     yield keywords
 
 
-sourcedata = queryiterator(workpath+"usen_queries_20170619_v2.tsv")
+#sourcedata = queryiterator(workpath+"usen_queries_20170619_v2.tsv")
+sourcedata = queryiterator(workpath+"usenqueries1M.tsv")
 
 
 if usephrase == False:
@@ -31,9 +33,9 @@ else:
     phrasebin = gensim.models.Phrases(sourcedata)
     model = gensim.models.Word2Vec(phrasebin[sourcedata], min_count=minimum, workers=workers, size=outsize)
 
-model.wv.save_word2vec_format(workpath+"testmodel.bin",binary=True)
+model.wv.save_word2vec_format(workpath+"testmodel_600.bin",binary=True)
 if txtoutput==True:
-    model.wv.save_word2vec_format(workpath+"testmodel.txt",binary=False)
+    model.wv.save_word2vec_format(workpath+"testmodel_600.txt",binary=False)
 
 
 
