e538ae0d716327bfe9530fd806ab03ad38a67cbc
maschram <maschram@microsoft.com>
2017-06-23 08:39:46 -0700

    Update feed_phrasew2e_tsne.py

--- feed_phrasew2e_tsne.py
+++ feed_phrasew2e_tsne.py
@@ -2,10 +2,13 @@ import gensim
 from nltk.corpus import stopwords
 from sklearn.manifold import TSNE
 from sklearn.cluster import KMeans
+from sklearn.cluster import Birch
+import time
 
-modelfilename = "c:\\users\\maschram\\downloads\\testmodel.bin"
+modelfilename = "c:\\users\\maschram\\downloads\\testmodel_300.bin"
 testqueryfilename = "c:\\users\\maschram\\downloads\\testqueries.tsv"
 outdim=3
+maxcount = 500
 stoplib=set(stopwords.words('english'))
 
 traineddata = gensim.models.KeyedVectors.load_word2vec_format(modelfilename, binary=True)
@@ -15,6 +18,7 @@ modelresult = [];
 #convert the list of queries into word vectors
 print("generating word vectors")
 counter=0
+t0 = time.clock()
 with open(testqueryfilename, encoding="utf8") as queryfile:
     for line in queryfile:
         for phrase in line.split("\t")[0].split("|"):
@@ -31,30 +35,53 @@ with open(testqueryfilename, encoding="utf8") as queryfile:
                 modelresult.append( [phrase, wordvec] )
                
         counter+=1
-        if(counter>50):
+        if(counter>maxcount and maxcount!=-1):
            break
 
 wordveclist = [x[1] for x in modelresult]
+print(str(time.clock()-t0)+" seconds")
 
 #create the t-sne model and fit points
 print("generating tsne")
+t0=time.clock()
 tsnemodel = TSNE(n_components = outdim, random_state = 0)
 tsnevec = tsnemodel.fit_transform(wordveclist)
+print(str(time.clock()-t0)+" seconds")
+
+##cluster points together
+#print("generating clusters")
+#t0=time.clock()
+#clustermodel = KMeans(n_clusters=12, random_state=0)
+#tsnecluster = clustermodel.fit_predict(tsnevec)
+
+#index = 0
+#for index in range(len(modelresult)):
+#    modelresult[index].append(tsnevec[index])
+#    modelresult[index].append(tsnecluster[index])
+#print(str(time.clock()-t0)+" seconds")
+
 
 #cluster points together
-print("generating clusters")
-clustermodel = KMeans(n_clusters=12, random_state=0)
+print("generating clusters - Birch clustering")
+t0=time.clock()
+clustermodel = Birch(threshold=1, n_clusters=None)
 tsnecluster = clustermodel.fit_predict(tsnevec)
+#clustermodel = KMeans(n_clusters=12, random_state=0)
+#tsnecluster = clustermodel.fit_predict(tsnevec)
 
 index = 0
 for index in range(len(modelresult)):
     modelresult[index].append(tsnevec[index])
     modelresult[index].append(tsnecluster[index])
+print(str(time.clock()-t0)+" seconds")
+
 
 
 #sort by cluster
+t0=time.clock()
 print('sorting by clusters')
 modelresult.sort( key=lambda x: x[3] )
+print(str(time.clock()-t0)+" seconds")
 
 
 for mr in modelresult:
