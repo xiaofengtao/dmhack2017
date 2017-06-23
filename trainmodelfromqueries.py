2a54361d45451410e9a0efc60d58dd8b23af4d30
maschram <maschram@microsoft.com>
2017-06-22 20:33:00 -0700

    Update trainmodelfromqueries.py

--- trainmodelfromqueries.py
+++ trainmodelfromqueries.py
@@ -1,22 +1,25 @@
-#frm gensim.models.keyedvectors import KeyedVectors
 import gensim
+from nltk.corpus import stopwords
 
 workpath = "c:\\users\\maschram\\downloads\\"
-usephrase = True
+usephrase = False
 outsize=200
 minimum=10
 workers=4
-binoutput = True
+txtoutput = True
 
 class queryiterator(object):
     def __init__ (self, filename):
         self.filename = filename
+        self.stop = set(stopwords.words('english'))
 
     def __iter__(self):
         with open(self.filename, encoding="utf8") as modelfile:
             for line in modelfile:
                 for phrase in line.split("\t")[0].split("|"):
-                    yield phrase.split(" ")
+                    keywords = phrase.split(" ")
+                    keywords = [k for k in keywords if k not in self.stop]
+                    yield keywords
 
 
 sourcedata = queryiterator(workpath+"usen_queries_20170619_v2.tsv")
@@ -28,9 +31,8 @@ else:
     phrasebin = gensim.models.Phrases(sourcedata)
     model = gensim.models.Word2Vec(phrasebin[sourcedata], min_count=minimum, workers=workers, size=outsize)
 
-if binoutput==True:
-    model.wv.save_word2vec_format(workpath+"testmodel.bin",binary=True)
-else:
+model.wv.save_word2vec_format(workpath+"testmodel.bin",binary=True)
+if txtoutput==True:
     model.wv.save_word2vec_format(workpath+"testmodel.txt",binary=False)
 
 
